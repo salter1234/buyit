2eb1037611aaa045c8748958b596117808a4d999
salter1234 <143507366+salter1234@users.noreply.github.com>
2023-08-29 20:31:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,6 +14,37 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('Y2UuAgi/bc4WdberD8e1IkEOJSzd0wFepokgM+LV2QdVuz5ll+sBo+rKG8xjJApTiQmoGlhP0AQ/9anLlc+2i0K0gfHQInI7iu14xNkGUg3aZI3ry11t3a6VU/noC4w5OFH3ie3BrJq9GQnGcevmWQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('d46ff916260630f6ed023338a7c33976')
 
+def about_us_event(event):
+    emoji = [
+            {
+                "index": 0,
+                "productId": "5ac21184040ab15980c9b43a",
+                "emojiId": "225"
+            },
+            {
+                "index": 17,
+                "productId": "5ac21184040ab15980c9b43a",
+                "emojiId": "225"
+            }
+        ]
+
+    text_message = TextSendMessage(text='''$ Master RenderP $
+Hello! 您好，歡迎您成為 Master RenderP 的好友！
+
+我是Master 支付小幫手 
+
+-這裡有商城，還可以購物喔~
+-直接點選下方【圖中】選單功能
+
+-期待您的光臨！''', emojis=emoji)
+
+    sticker_message = StickerSendMessage(
+        package_id='8522',
+        sticker_id='16581271'
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, sticker_message])
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -36,7 +67,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #event有什麼資料？詳見補充
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id
+    message_text = str(event.message.text).lower()
 
+    ##################使用說明 選單 油價查詢###############
+    if message_text == '@使用說明':
+        about_us_event(event)
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text='Hi! Welcome to LSTORE.'))
     
